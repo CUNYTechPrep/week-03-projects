0c461dd382e67c8258ff0f88f55c93d8828ba967
Edgardo <medgardo@gmail.com>
2017-09-12 17:06:58 -0400

    Add starter code for project 01

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function City(props) {
+  return (<div></div>);
+}
+
+function ZipSearchField(props) {
+  return (<div></div>);
+}
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Zip Code Search</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ZipSearchField />
+        <div>
+          <City />
+          <City />
+        </div>
       </div>
     );
   }
