13aa15ce535119596697d850a0bcfd96e1177679
Moustafa <drsh2k19@gmail.com>
2017-09-15 19:42:31 -0400

    completed zip-search

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,25 +4,85 @@ import './App.css';
 
 
 function City(props) {
-  return (<div></div>);
+  const dataArray = Object.keys(props.data).map((key, i) => {
+    if (i < 8)
+      return <li>{ key + ': ' + props.data[key] } </li>
+  });
+
+  let style = {'fontWeight':'bold'}
+  return (<div className="row">
+    <div
+    className="col-lg-4 col-lg-offset-4 col-md-offset-4 col-sm-offset-4 col-md-4 col-sm-4 panel panel-default text-center">
+     <div style={style} className="panel-heading">{props.data.City}, {props.data.State}</div>
+      <div  className="panel-body text-left ">
+      <ul>
+      {dataArray}
+      </ul>
+      </div>
+    </div>
+    </div>
+  );
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (<div className="container">
+
+    <div className="row">
+
+    <div className=" col-lg-12 text-center">
+      <label>Zip Code:</label>
+      <input type="text" placeholder="Enter Zip Code"
+             onChange={props.changeHandler} value={props.value} />
+    </div>
+    </div>
+    </div>
+  );
 }
 
 
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      zipCode : '',
+      cities: [],
+    };
+    this.zipCodeChanged = this.zipCodeChanged.bind(this);
+  }
+
+
+  zipCodeChanged(evt){
+    const zip = evt.target.value;
+    if (zip.length !== 5)
+      return;
+    console.log('event');
+    fetch('http://ctp-zip-api.herokuapp.com/zip/'+ zip).then((response) =>{
+      return response.json();
+    }).then((jsonResponse) => {
+      console.log(jsonResponse);
+      this.setState({
+          cities : jsonResponse.map((data) => {
+          return (<City data={data} />);
+        }),
+      });
+    }).catch((err) =>{
+      this.setState({cities:[]});
+      console.log(err);
+    });
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
+        <ZipSearchField changeHandler={this.zipCodeChanged} />
+        <div className="container" >
+          {this.state.cities}
         </div>
       </div>
     );
