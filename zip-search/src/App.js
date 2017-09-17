5e4deefcbe2f6b4e4c7d562fe9edbf87e4293289
Samuel Witke <witkesam@gmail.com>
2017-09-17 11:13:31 -0400

    Project 1 Zip Code Search App

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,27 +4,70 @@ import './App.css';
 
 
 function City(props) {
-  return (<div></div>);
+	function numberWithCommas(x) {
+		console.log(x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
+	let pop = numberWithCommas(props.data.EstimatedPopulation);
+	let wages = numberWithCommas(props.data.TotalWages );
+  return (<div className="panel panel-default">
+			<div className="panel-heading">{props.data.City}</div>  
+			<div className="panel-body">
+			<h4> Population: {pop}</h4>
+			<h4> TotalWages: {wages} </h4>
+			<h4> TaxReturnsFiled: {props.data.TaxReturnsFiled} </h4>
+			</div>
+		</div>);
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+	return (<div>
+			<input type="text" placeholder="Try 10016" value={props.value} onChange={ (e) => { props.handleChange(e); } } />
+			</div>);
 }
 
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      zipCode: '',
+      cities: []
+    };
+	this.getZip = this.getZip.bind(this); 
+  }
+  getZip(e) {
+	var zip = e.target.value;
+	this.setState({
+		zipCode: zip, 
+    });
+	if(zip.length == 5) {
+		fetch("http://ctp-zip-api.herokuapp.com/zip/"+zip)
+		.then( (responce) => {
+			return responce.json();
+		})
+		.then( (jsonData) => {
+			var citiesJson = jsonData.map( (obj) =>{
+				console.log(obj);
+				return <City data={obj} />;
+			});
+			this.setState({
+				cities: citiesJson,
+			});
+		});
+	}
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Zip Code Search</h2>
-        </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
-        </div>
-      </div>
+        	<div className="App-header">
+          		<h2>Zip Code Search</h2>
+			</div>
+			<div className="container">
+				<label><h2> Zip Code: </h2></label>
+        		<ZipSearchField value={this.state.zipCode} handleChange = {this.getZip} />
+				{this.state.cities}
+        	</div>
+     </div>
     );
   }
 }
