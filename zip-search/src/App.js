ebfb926a784a3ff49651b6d93d1259a041d555f8
Brian Campbell <brian_campbell@outlook.com>
2017-09-17 22:28:00 -0400

    ZipFinder done, need to adjust CSS

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,25 +4,78 @@ import './App.css';
 
 
 function City(props) {
-  return (<div></div>);
+  return (
+    <div className="panel panel-default">
+      <div className="panel-heading">
+        {props.data.City}
+      </div>
+      <div className = "panel-body">
+        <ul>
+          <li>State: {props.data.State}</li>
+          <li>Location: ({props.data.Lat}, {props.data.Long})</li>
+          <li>Population (estimated): {props.data.EstimatedPopulation}</li>
+          <li>Total Wages: {props.data.TotalWages}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (
+    <div>
+      <label>Zip Code:</label>
+      <input type="text" placeholder = "Try 11413" onChange={props.handleChange} value={props.value}/>
+    </div>
+  );
 }
 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      zipCode: "",
+      cities: []
+    };
+
+    this.zipChanged = this.zipChanged.bind(this);
+  }
+
+  zipChanged(event) {
+    const zip =  event.target.value
+    this.setState({
+      zipCode: zip
+    });
+
+    if(zip.length === 5){
+      fetch("http://ctp-zip-api.herokuapp.com/zip/" + zip)
+        .then((response) => {
+          return response.json();
+        })
+        .then((jsonData) => {
+          const cities = jsonData.map((obj)=><City data={obj} />);
+          this.setState({
+            cities: cities
+          })
+        });
+
+
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <br/>
+        <ZipSearchField value={this.state.zipCode} handleChange={this.zipChanged}/>
+        <br/>
         <div>
-          <City />
-          <City />
+         {this.state.cities}
         </div>
       </div>
     );
