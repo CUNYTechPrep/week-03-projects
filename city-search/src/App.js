f23a33fe11e5c79ec313fab477f65b6fdfdb8ff2
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-09-18 20:05:03 -0400

    More information is printed for each city

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -12,16 +12,58 @@ function CitySearchField(props) {
 }
 
 class ZipCodes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cityInfo: {},
+    }
+    this.getCityInfoFromZip = this.getCityInfoFromZip.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCityInfoFromZip();
+  }
+
+  getCityInfoFromZip() {
+    const cityArr = [];
+    fetch('http://ctp-zip-api.herokuapp.com/zip/' + this.props.zip)
+      .then( (response) => {
+        return response.json();
+      })
+      .then( (jsonBody) => {
+        console.log(jsonBody);
+
+        jsonBody.forEach( obj => {
+          if(obj.City === this.props.city.toUpperCase()) {
+            this.setState({
+              cityInfo: {
+                state: obj.State,
+                coordinates: '(' + obj.Lat + ', ' + obj.Long + ')',
+                population: obj.EstimatedPopulation,
+                totalWages: obj.TotalWages,
+              }
+            })
+          }
+        });
+        //console.log(cityInfo);
+      });
+
+      console.log(this.state.cityInfo.state);    
+  }
+
   render() {
     return(
       <div className="container">
         <div className="panel panel-default">
           <div className="panel-heading">
-            <p>Zip Codes</p>
+            {this.props.zip}
           </div>
           <div className="panel-body">
             <ul>
-              {this.props.zipList.map( (zipCode) => <li>{zipCode}</li>)}
+              <li>State: {this.state.cityInfo.state}</li>
+              <li>Estimated Population: {this.state.cityInfo.population}</li>
+              <li>Location: {this.state.cityInfo.coordinates}</li>
+              <li>Total Wages: {this.state.cityInfo.totalWages}</li>
             </ul>
           </div>
         </div>
@@ -53,8 +95,7 @@ class App extends Component {
       })
       .then((jsonBody) => {
         console.log(jsonBody);
-        //const tempArry = jsonBody.map( (zip) => zip);
-        const zipCodeList = <ZipCodes zipList={jsonBody} />;
+        const zipCodeList = jsonBody.map( (zipCode) => <ZipCodes zip={zipCode} city={this.state.city}/>);
         this.setState({
           ZipCodes: zipCodeList,
         })
