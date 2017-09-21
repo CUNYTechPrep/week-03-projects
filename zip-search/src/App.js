6d922a232dc25342813041241b5f62e6126a33f3
Erii Sugimoto <eriisugimoto1293@gmail.com>
2017-09-21 18:32:15 -0400

    complete

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,25 +4,73 @@ import './App.css';
 
 
 function City(props) {
-  return (<div></div>);
+  return (
+    <div className="container">
+      <div className="panel-group col-md-6 col-sm-12 col-md-offset-3">
+        <div className="panel panel-primary">
+          <div className="panel-heading">{props.data.City}</div>
+          <div className="panel-body">
+            <ul>
+              <li>State: {props.data.State}</li>
+              <li>Location: {props.data.Location}</li>
+              <li>Population: {props.data.EstimatedPopulation}</li>
+              <li>Total Wages: {props.data.TotalWages}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (
+  <div>
+    <label>Zip Code: </label>
+    <input type="text" placeholder="Try 10013" onChange={props.handleClick}/>
+  </div>
+  );
 }
 
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      zipCode: "",
+      cities: []
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const zip = event.target.value;
+    
+    if(zip.length === 5){
+
+      fetch("http://ctp-zip-api.herokuapp.com/zip/" + zip)
+      .then(response => {return response.json();
+      }).then((jsonData) => {
+        const output = jsonData.map(city => {
+          return <City data={city} />;
+        });
+
+        this.setState({
+            cities: output
+        });
+
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField handleClick={this.handleClick}/>
         <div>
-          <City />
-          <City />
+          {this.state.cities}
         </div>
       </div>
     );
