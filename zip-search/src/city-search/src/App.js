992e3997f2d706fa0431bef1c1a2aca2e6333e0b
Shelly Huang <shellyhuang81@gmail.com>
2017-09-20 23:58:41 -0400

    finished city search

---
+++ zip-search/src/city-search/src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function ZipCodes(props){
+  return(
+    <div className="row">
+      <div className="col-xs-12">
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">{props.data.LocationText}</h3>
+          </div>
+          <div className="panel-body">
+            <ul>
+              <li>{props.data}</li>
+            </ul>
+          </div>
+        </div>
+      </div>    
+    </div>
+  );
+}
+
+function CityField(props){
+  return(
+    <div>
+      <h3>Enter a City: </h3>
+      <input type='text' 
+             value={props.city} 
+             onChange={props.handleChange}>
+      </input>
+      <button onClick={props.handleClick}>Submit</button> 
+    </div>
+  );
+}
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      zips: [],
+      city: null
+    }
+    this.cityChanged = this.cityChanged.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  };
+
+  cityChanged(event){
+    const city = event.target.value;
+    this.setState({
+      city: city,
+    });
+  }
+
+  handleClick(){
+    const city = this.state.city;
+
+    fetch("http://ctp-zip-api.herokuapp.com/city/"+city.toUpperCase())
+    .then((response) => {
+    return response.json();
+    })
+    .then((jsonData) => {
+      const zips = jsonData.map((obj) => 
+      	{ return <ZipCodes data={obj}/>; }
+      );
+      
+      this.setState({
+        zips: zips,
+      })
+      console.log(this.state.zips);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+        	<h3>City Search </h3>
+        </div>
+        <CityField 
+        	value={this.state.city} 
+            handleChange={this.cityChanged} 
+            handleClick={this.handleClick}/>
+         <div>
+           {this.state.zips}
+         </div>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
