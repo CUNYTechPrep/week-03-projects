7959c2b9a39b93bf3989d6404bc62511e1ba0790
Kahalia Stanberry <kahalia.stanberry@lc.cuny.edu>
2017-09-22 17:00:52 -0400

    zip2

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,25 +4,96 @@ import './App.css';
 
 
 function City(props) {
-  return (<div></div>);
+
+  return (
+    <div className= "panel panel-default">
+
+      <div className= "panel-heading"> 
+      {props.data.City}
+      </div>
+
+      <div className= "panel-body"> 
+      <ul>
+
+      
+      <li>State: {props.data.State}</li>
+      <li>Location: {props.data.Location}</li>
+      <li>Population (estimated): {props.data.EstimatedPopulation}</li>
+      <li>Total Wages: {props.data.TotalWages}</li>
+
+
+      </ul>
+      </div>
+
+    </div>
+    )
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (<div>
+    <label>Zip Code:</label>
+    <input type ="text" className="form-control" onChange={props.handleChange} value ={props.value}/>
+
+  </div>);
 }
 
 
 class App extends Component {
+  constructor(){
+    //we add state in the constructor. what have to call super.
+    super();
+    this.state ={
+     
+      zipCode: "",
+      city:[<City/>]
+
+    };
+
+    this.zipCodeChanged = this.zipCodeChanged.bind(this);
+  }
+
+  zipCodeChanged(event){
+
+    const zip = event.target.value;
+
+    if (zip.length === 5 ){
+
+      fetch('http://ctp-zip-api.herokuapp.com/zip/'+zip)
+        .then((response)=>{
+
+          return response.json();
+        })
+        .then((jsonBody)=>{
+          console.log(jsonBody);
+
+          const cityComps = jsonBody.map((city)=> <City data={city}/> );
+
+          this.setState({
+
+            cities: cityComps
+
+          });
+
+        })
+
+
+    }
+
+    this.setState({
+      zipCode: zip
+    })
+
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
-        </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
+      </div>
+      <ZipSearchField handleChange={this.zipCodeChanged} value={this.state.zipCode}/>
+      <div>
+          {this.state.cities}
+
         </div>
       </div>
     );
