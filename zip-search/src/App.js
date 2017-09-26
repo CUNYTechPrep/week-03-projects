d9767dff0d6e7d3b6381bb2a724f0e0cf599b466
Akbar Mirza <akbarbmirza@gmail.com>
2017-09-26 01:21:00 -0400

    Complete Zip Code Search App

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,25 +4,100 @@ import './App.css';
 
 
 function City(props) {
-  return (<div></div>);
+
+  const record = props.record;
+  // console.log(City, State, Location, Long, Lat, EstimatedPopulation, TotalWages);
+
+  return (
+    <div className="panel panel-default">
+      <div className="panel-heading">{record.LocationText}</div>
+      <div className="panel-body">
+        <ul>
+          <li>State: {record.State}</li>
+          <li>Location: ({record.Long}, {record.Lat})</li>
+          <li>Population (Estimated): {record.EstimatedPopulation}</li>
+          <li>Total Wages: {record.TotalWages}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (
+    <label htmlFor="zip">
+    Zip Code: 
+    <input name="zip"
+           type="text"
+           placeholder="Enter a Zip Code..."
+           value={props.value}
+           onChange={props.onChange} />
+    </label>
+  );
 }
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', data: []};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.fetchZipCode = this.fetchZipCode.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({value: event.target.value});
+    console.log(this.state);
+  }
+
+  handleSubmit(event) {
+    this.fetchZipCode(this.state.value);
+    
+    event.preventDefault();
+  }
+
+  fetchZipCode(zip) {
+    const API = `http://ctp-zip-api.herokuapp.com/zip/${zip}`
+    // GET JSON
+    fetch(API)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log('parsed JSON', json);
+        this.setState({
+          data: json
+        });
+      })
+      .catch((ex) => {
+        console.log('parsing failed', ex);
+      })
+  }
+  
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
+        <div className="container">
+          <form
+            onSubmit={this.handleSubmit} >
+            <ZipSearchField
+              value={this.state.value}
+              onChange={this.handleChange} />
+          </form>
+
+          <div>
+            {console.log("TEST")}
+            {console.log(this.state.data)}
+            { this.state.data.map((record, index) => {
+              return <City key={index} record={record} />;
+            }, this) }
+          </div>
         </div>
       </div>
     );
