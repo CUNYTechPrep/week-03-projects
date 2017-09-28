03f6f7dc01d09cd1ec662e366bc41346093ac153
Joseph Huaynate <jahuay93@gmail.com>
2017-09-28 00:45:02 -0400

    Update App.js

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,25 +4,79 @@ import './App.css';
 
 
 function City(props) {
-  return (<div></div>);
+  return (
+    <div className="panel panel-default">
+       <div className="panel-heading">
+          <h5>{props.data.City} , {props.data.State}</h5>
+        </div>
+        <div className="panel-body">
+          <ul>
+            <li>State: {props.data.State}</li>
+            <li>Location: ({props.data.Lat} , {props.data.Long})</li>
+            <li>Est. Population: {props.data.EstimatedPopulation}</li>
+            <li>Total Wages: {props.data.TotalWages}</li>
+          </ul>
+        </div>
+    </div>);
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (<div className="zip-field">
+    <label> Zip Code:</label>
+      <input type="text" onChange={props.handleChange} value={props.value} placeholder= "Try 10016"/>
+    </div>);
 }
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state= {
+      zipCode: "",
+      cities: [] ,
+    }
+    this.zipCodeChanged = this.zipCodeChanged.bind(this);
+  }
+
+  zipCodeChanged(event) {
+    const zip = event.target.value;
+
+    const noResults = (
+      <p>No Results</p>
+    );
+
+    if(zip.length === null) {
+      return noResults;
+    } else if(zip.length === 5) {
+    fetch('http://ctp-zip-api.herokuapp.com/zip/' + zip)
+      .then((response)=> {
+        return response.json();
+      })
+      .then((jsonBody) => {
+        console.log(jsonBody);
+
+      const cityComps= jsonBody.map((c) => <City data={c} />);
+
+      this.setState({
+        cities: cityComps
+        });
+      });
+    }
+
+    this.setState({
+      zipCode: zip,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField handleChange={this.zipCodeChanged} value={this.state.zipCode}/>
         <div>
-          <City />
-          <City />
+          {this.state.cities}
         </div>
       </div>
     );
@@ -30,3 +84,4 @@ class App extends Component {
 }
 
 export default App;
+
