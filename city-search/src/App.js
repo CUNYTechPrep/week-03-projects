9f5b461e8a95115512c6884e032dd1fbaab7dea8
Abigail Banting <abanting00@gmail.com>
2017-09-28 18:15:45 -0400

    Added city search

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -1,30 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function Zip(props) {
-  return(
-    <div></div>
+
+  return (
+    <div className="panel panel-default">
+      <p>{props.zipData}</p>
+    </div>
   );
 }
 
 function CitySearchField(props) {
-  return(
-    <div></div>
+  return (
+    <div className="row">
+      <div className="col-xs-12">
+        City:
+        <input value ={props.cityVal} onChange={props.cityChange}></input>
+      </div>
+    </div>
   );
 }
 
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      city: '',
+      data: [<div>No Result</div>]
+    };
+
+    this.updateCity = this.updateCity.bind(this);
+  }
+
+  updateCity(evt){
+
+    let currentCity = evt.target.value;
+    this.setState({
+      city: currentCity
+    })
+    
+    fetch('http://ctp-zip-api.herokuapp.com/city/' + currentCity)
+      .then((resp) => {
+        if(resp.ok){
+          return resp.json();
+        }else{
+          return [];
+        }
+      }).then((respJson) => {
+        const zip = respJson.map(d => {
+          return <Zip zipData={d} />
+        })
+
+        this.setState({
+          data: zip
+        })
+      })
+  }
+
+
   render() {
+    console.log(this.state.data);
     return (
       <div className="App">
         <div className="App-header">
-            <h2>Welcome to React</h2>
+          <h2>Zip Code Search</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-xs-6">
+              <CitySearchField 
+                cityVal={this.state.city} 
+                cityChange = {this.updateCity}
+              />
+                {this.state.data}
+            </div>
+        </div>
       </div>
+    </div>
     );
   }
 }
