f7efd365835600110f845a3c7a2bb9fba8a3977c
Vish Misir <vmisir000@citymail.cuny.edu>
2017-10-02 00:16:23 -0400

    update component

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -8,21 +8,53 @@ function City(props) {
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (
+    <div className="row">
+      <div className="col-xs-12 form-inline">
+        <label htmlFor="zip">Zip Code: </label>
+        <input
+          type="text"
+          id="zip"
+          className="form-control"
+          value={props.zipValue}
+          onChange={props.handleChange}
+          placeholder="Try 11418" />
+      </div>        
+    </div>
+  );
 }
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      zipValue: "",
+      cities: [],
+    }
+
+    this.zipValueChanged = this.zipValueChanged.bind(this);
+  }
+
+  zipValueChanged(event) {
+    const zip = event.target.value;
+
+    this.setState({zipValue: zip});
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
+        <div className="container-fluid">
+          <div className="row">
+            <ZipSearchField 
+              zipValue={this.state.zipValue}
+              handleChange={this.zipValueChanged} />
+        
+          </div>
         </div>
       </div>
     );
