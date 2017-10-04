664449d2d713252eac7d506ae38bbc90e97e8676
Ken Zou <kenzou1944@gmail.com>
2017-10-04 16:44:02 -0400

    Finished The Week-03 Project

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,25 +4,80 @@ import './App.css';
 
 
 function City(props) {
-  return (<div></div>);
+  return (<div className='panel panel-default'>
+    <div className='panel-heading'>
+    {props.data.City}
+    </div>
+
+    <div className='panel-body'>
+   <ul>
+   <li>City: {props.data.City}</li>
+   </ul>
+    </div>
+
+  </div>);
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (<div>
+    <label>Zip Code</label>
+    <input type='text' onChange={props.handleChange} value={props.value}/>
+  </div>);
 }
 
 
 class App extends Component {
+  constructor()
+  {
+    super();
+    this.state={
+      cityname:"",
+      zipCode: "",
+      cities : []
+    };
+    this.zipCodeChange = this.zipCodeChange.bind(this); 
+  }
+
+  zipCodeChange(event)
+  {
+    
+    const zip = event.target.value;
+
+    if(zip.length === 5)
+    {
+      fetch('http://ctp-zip-api.herokuapp.com/zip/' + zip)
+      .then((response) =>{
+        return response.json();
+      })
+      .then((jsonBody) => {
+        console.log(jsonBody);
+
+        const cityComps = jsonBody.map((city) => <City data ={city}/>)  //create punch of citis tag from the city array and pass entire city to the data
+     
+        this.setState(
+        {
+          cities: cityComps
+        });
+      })
+    }
+
+    this.setState(
+    {
+      zipCode: zip
+    });
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField handleChange={this.zipCodeChange} value={this.state.zipCode}/>
+
         <div>
-          <City />
-          <City />
+          {this.state.cities}
         </div>
       </div>
     );
